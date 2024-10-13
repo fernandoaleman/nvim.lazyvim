3492fda467e2a50897f15b2f066f539004d2c9e7
Fernando Alemán <fernandoaleman@mac.com>
2024-10-13 18:40:32 -0500

    Initial commit

---
+++ lua/plugins/catppuccin.lua
@@ -0,0 +1,21 @@
+return {
+  {
+    "catppuccin/nvim",
+    lazy = false,
+    name = "catppuccin",
+    priority = 1000,
+    config = function()
+      require("catppuccin").setup({
+        flavour = "mocha",
+        transparent_background = true,
+        styles = {
+          comments = { "italic" },
+        },
+      })
+
+      vim.o.termguicolors = true
+      vim.o.background = "dark"
+      vim.cmd.colorscheme("catppuccin")
+    end,
+  },
+}
