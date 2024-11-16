e5191f1f12713bd1f64ed3442dcae7d5547725c5
Fernando Alemán <fernandoaleman@mac.com>
2024-11-16 17:42:32 -0600

    Add toml plugin

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -24,6 +24,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.markdown" },
     { import = "lazyvim.plugins.extras.lang.python" },
     { import = "lazyvim.plugins.extras.lang.ruby" },
+    { import = "lazyvim.plugins.extras.lang.toml" },
     { import = "lazyvim.plugins.extras.lang.yaml" },
     { import = "plugins" },
   },
